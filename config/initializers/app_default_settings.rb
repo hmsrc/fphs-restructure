d4e67c39bc7bef6fef83623e0b758b5b54003643
Restructure Build Process <phil_ayres@hms.harvard.edu>
2025-04-09 19:01:05 +0000

    Built and tested release-ready version '9.12.1'

--- config/initializers/app_default_settings.rb
+++ config/initializers/app_default_settings.rb
@@ -23,7 +23,7 @@ class DefaultSettings
   DefaultMigrationSchema = 'ml_app'
   DbPrefix = 'restr'
   GlobalIdPrefix = 'fpa1'
-  CaptionAcronyms = %w[IPA IPAs BHS PI PIs HMS FPHS MD RA RAs].freeze
+  CaptionAcronyms = %w[IPA IPAs BHS PI PIs HMS FPHS MD RA RAs ID].freeze
   # Rails.logger levels to use for errors logged in AppExceptionHandler
   # Any unlisted method will default to level :error
   LogLevel = {
