157cfad3f67244dc5e8d10d49125f9f98d202e9a
Restructure Build Process <phil_ayres@hms.harvard.edu>
2026-06-17 16:29:41 +0000

    Built and tested release-ready version '9.44.12' - dev repo

--- db/dumps/current_schema.sql
+++ db/dumps/current_schema.sql
@@ -3,7 +3,7 @@ begin;
 -- PostgreSQL database dump
 --
 
-\restrict XJdccQqSR9NjPDVFza0QIZgH57dpc9uVYADFE3U4hkaJ4ewkLVwPtrNCE6VEyeu
+\restrict 2aPQCPb0aeZtXZXzpmbekxFch7pYhsbH1d7audRZSUaKJySRckF2uqWXOKwTEYt
 
 -- Dumped from database version 15.16
 -- Dumped by pg_dump version 15.16
@@ -23356,6 +23356,6 @@ ALTER TABLE ONLY ref_data.redcap_data_dictionary_history
 -- PostgreSQL database dump complete
 --
 
-\unrestrict XJdccQqSR9NjPDVFza0QIZgH57dpc9uVYADFE3U4hkaJ4ewkLVwPtrNCE6VEyeu
+\unrestrict 2aPQCPb0aeZtXZXzpmbekxFch7pYhsbH1d7audRZSUaKJySRckF2uqWXOKwTEYt
 
 commit;
